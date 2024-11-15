82991985ed606a4b01be965fc0884445ff976afc
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-11-15 22:33:24 +0100

    fix card shop

--- source/js/addToCard.js
+++ source/js/addToCard.js
@@ -1,14 +1,9 @@
 const showCards = document.querySelector('.show-cards');
 const btnShop = document.querySelectorAll('.btn-shop');
 
-JSON.parse(localStorage.getItem('addCardsToLocal')) || []
-let localCards = JSON.parse(localStorage.getItem('addCardsToLocal')) || []
 for (btn of btnShop) {
             
     btn.addEventListener('click', () => {
-        if(localCards.length == 0) {
-            return
-        }
         showCards.classList.toggle('show')
     })
 }
