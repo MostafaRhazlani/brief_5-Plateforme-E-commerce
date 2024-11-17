c0c34b55997f729aa5f89046206650ca15f9c7af
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-11-17 04:48:02 +0100

    delet all comment i add for nothing

--- source/js/affichageProduct.js
+++ source/js/affichageProduct.js
@@ -300,39 +300,6 @@ sort.addEventListener("change",()=>{
    
 });
 //max et min price
-// let maxpriceshow = document.getElementById("maxpriceshow");
-// let minprice = document.getElementById("minprice");
-// let maxprice = document.getElementById("maxprice");
-// let rest = document.getElementById("rest");
-
-// function filterProductsByPrice(min, max) {
-//   let filtr = products.filter(product => 
-//       product.price >= min && product.price <= max
-//   );
-//   fetchProducts(filtr,0);
-// }
-
-// let maxProductPrice = Math.max(products.map(p => p.price));
-// maxpriceshow.textContent = `$${maxProductPrice.toFixed(2)}`;
-// maxprice.value = maxProductPrice;
-
-// minprice.addEventListener("input", () => {
-//   let min = parseFloat(minprice.value) || 0;
-//   let max = parseFloat(maxprice.value) || maxProductPrice;
-//   filterProductsByPrice(min, max);
-// });
-// maxprice.addEventListener("input", () => {
-//   let min = parseFloat(minprice.value) || 0;
-//   let max = parseFloat(maxprice.value) || maxProductPrice;
-//   filterProductsByPrice(min, max);
-// });
-
-rest.addEventListener("click", () => {
-  minprice.value = 0;
-  maxprice.value = maxProductPrice;
- 
-});
-
 minprice.addEventListener('input', () => {
     let minPriceValue = minprice.value;
     let maxPriceValue = maxprice.value;
